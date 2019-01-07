3e24efee2671540ad88dc8bb18209f5b5d4f3d3d
mina marcos <minamarcos@gmail.com>
2019-01-06 18:49:06 -0800

    Updates

---
+++ precache-manifest.26499b316107f297dcf632cc50bc778b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5129a64e2be4b025a08",
+    "url": "/reactappthestage/static/js/runtime~main.b5129a64.js"
+  },
+  {
+    "revision": "2368a9b3de3ddccf1869",
+    "url": "/reactappthestage/static/js/main.2368a9b3.chunk.js"
+  },
+  {
+    "revision": "195dbea69e3ea5a80424",
+    "url": "/reactappthestage/static/js/1.195dbea6.chunk.js"
+  },
+  {
+    "revision": "2368a9b3de3ddccf1869",
+    "url": "/reactappthestage/static/css/main.3106ba9f.chunk.css"
+  },
+  {
+    "revision": "b7dadad8e021f5680e7fd80169fdf3ee",
+    "url": "/reactappthestage/index.html"
+  }
+];
